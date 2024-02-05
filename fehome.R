c0d71ab581d841087cdde308548e0b0b2b5bb14e
shchalla <111800924+shchalla@users.noreply.github.com>
2024-02-05 13:13:14 -0600

    Update fehome.R

--- fehome.R
+++ fehome.R
@@ -1,94 +1,73 @@
-library(haven) 
-library(dplyr) 
-install.packages("sjlabelled")
-library(sjlabelled) # for extracting labels
-library(stargazer)
-
-# Change this to the path for the folder in which you saved the data
-#setwd("/Users/shreyachalla/Downloads/gss.RDS")
-file_path <- "/Users/shreyachalla/Downloads/gss.RDS"
-
-# Extract the directory
-directory <- dirname(file_path)
-
-# Set the working directory to the extracted directory
-setwd(directory)
-
-# Read the data from RDS file
+setwd("/Users/shreyachalla/Downloads/gss.RDS")
+library(haven)
+library(dplyr)
+library(sjlabelled)
 gss<- readRDS("gss.RDS")
 
-attr(gss$fehome,"labels")
-table(gss$fehome)
-gss2 <- gss[complete.cases(gss$fehome), ]
+library(ggplot2)
+library(ggthemes)
+theme_set(theme_tufte())
 
-
-
-library(ggplot2) # Load ggplot2 library for plotting
-library(ggthemes) # Load ggthemes library for themes
-theme_set(theme_tufte()) # This is my favorite theme, but you can use any other
-
-result <- gss2 %>%
-  group_by(year) %>%
-  summarize(fehomeper = sum(fehome == 2) / n() * 100)
-
-# Merge the result back to the original data frame
-gss2 <- merge(gss2, result, by = "year", all.x = TRUE)
-#gss$fehomeper
-ggplot(gss2,aes(x=year,y=fehomeper))+
-  geom_smooth()+
-  labs(y = "% disagree")
-
-table(gss$fehome,gss$year)
-table(gss2$fehome,gss2$year)
-validgss<- gss2[gss2$year %in% c(1974, 1998), ]
-
-gss_small <-  gss2[gss2$year <= 1980  | gss2$year >= 1992, ]
-gss_small$late <- as.numeric(gss_small$year==1998)
-gss_small$Female <- as.numeric(gss_small$sex==2)
-
-model1 <- lm(fehomeper~Female*late, gss_small)
-print(model1)
-stargazer(model1,header=FALSE,title="",type="text")
-
-#subset_data <- gss2[gss2$year %in% c(1978, 1998), ]
-#subset_data$year <- as.factor(subset_data$year)
-
-#ggplot(subset_data, aes(x = year, y = fehomeper, col = as.factor(sex))) +
-  #geom_smooth() +
-  #labs(y = "% disagree")
-
-ggplot(gss2[gss_small2$year==1978 | gss_small2$year==1998,],aes(x=year,y=fehomeper,col=as.factor(sex)))+ geom_smooth()+labs(y = "% disagree")
-
-#checking if they work for govt/military
-gss_small$govtjob <- as.numeric(gss_small$indus10>=9370)
-gss_small2 <- gss_small[complete.cases(gss_small$govtjob), ]
-
-ggplot(gss_small2[gss_small2$year==1978 | gss_small2$year==1998,], aes(x = year, y = fehomeper, col = as.factor(govtjob))) +
-  geom_smooth(method = 'gam', formula = y ~ s(x, bs = "cs", k = 5)) +
-  theme_minimal()+labs(y = "% disagree")
-
-gss_smaller2 <-  gss_small2[gss_small2$year <= 1980  | gss_small2$year >= 1992, ]
-gss_smaller2$late <- as.numeric(gss_smaller2$year>=1992)
-
-model2 <- lm(fehomeper~govtjob*late, gss_smaller2)
-print(model2)
-stargazer(model2,header=FALSE,title="",type="text")
-
-gss_small3<-gss2[gss2$partyid <=2 | gss2$partyid %in%c(3:6),]
-gss_small3<-gss_small3[,c('year','fehomeper','partyid')]
-gss_small3<-droplevels(gss_small3)
-gss_small3<-na.omit(gss_small3)
-gss_small3$Democrat <- gss_small3$partyid <=2
-ggplot(gss_small3,aes(x=year,y=fehomeper,col=Democrat))+ylab("% disagree")+
+# Sex
+smalldatashreya<-gss[,c('year','fehome','sex')]
+smalldatashreya<-droplevels(smalldatashreya)
+smalldatashreya<-na.omit(smalldatashreya)
+ggplot(smalldatashreya,aes(x=year,y=fehome,col=sex))+ylab("% disagree")+
   geom_smooth()+scale_colour_brewer(palette = "Set1")+
   theme(legend.position="bottom")
 
-# Difference in Difference
-gss_small3diff <-  gss_small3[gss_small3$year <=1980 | gss_small3$year >= 1992, ]
-gss_small3diff$late <- as.numeric(gss_small3diff$year >=1992)
-model3 <- lm(fehomeper~Democrat*late,gss_small3diff)
-stargazer(model3,header=FALSE,title="",type="text")
+library(stargazer)
+smalldatashreyab <-  smalldatashreya[smalldatashreya$year == 1974 | smalldatashreya$year == 1998, ]
+smalldatashreyab$late <- as.numeric(smalldatashreyab$year == 1998)
+smalldatashreyab$Female <- as.numeric(smalldatashreyab$sex == 2)
+modelshreya <- lm(fehome~Female*late,smalldatashreyab)
+stargazer(modelshreya,header=FALSE,title="",type="text")
 
+# Party ID
+smalldatashreya2<-gss[gss$partyid <=2 | gss$partyid %in%c(3:6),]
+smalldatashreya2<-smalldatashreya2[,c('year','fehome','partyid')]
+smalldatashreya2<-droplevels(smalldatashreya2)
+smalldatashreya2<-na.omit(smalldatashreya2)
+smalldatashreya2$Democrat <- smalldatashreya2$partyid <=2
+ggplot(smalldatashreya2,aes(x=year,y=fehome,col=Democrat))+
+  geom_smooth()+scale_colour_brewer(palette = "Set1")+
+  theme(legend.position="bottom")
+
+smalldatashreya2b <-  smalldatashreya2[smalldatashreya2$year == 1974 | smalldatashreya2$year == 1998, ]
+smalldatashreya2b$late <- as.numeric(smalldatashreya2b$year == 1998)
+modelshreya2 <- lm(fehome~Democrat*late,smalldatashreya2b)
+stargazer(modelshreya2,header=FALSE,title="",type="text")
+
+# Religious vs. Not
+gss$none <- gss$relig==4
+table(gss$none)
+smalldatashreya3<-gss[,c('year','fehome','none')]
+smalldatashreya3<-droplevels(smalldatashreya3)
+smalldatashreya3<-na.omit(smalldatashreya3)
+ggplot(smalldatashreya3,aes(x=year,y=fehome,col=none))+ylab("% disagree")+
+  geom_smooth()+scale_colour_brewer(palette = "Set1")+
+  theme(legend.position="bottom")
+
+smalldatashreya3b <-  smalldatashreya3[smalldatashreya3$year == 1974 | smalldatashreya3$year == 1998, ]
+smalldatashreya3b$late <- as.numeric(smalldatashreya3b$year == 1998)
+modelshreya3 <- lm(fehome~none*late,smalldatashreya3b)
+stargazer(modelshreya3,header=FALSE,title="",type="text")
+
+#checking if they work for govt/military
+gss$govtjob <- gss$indus10>=9370
+table(gss$govtjob)
+smalldatashreya4<-gss[,c('year','fehome','govtjob')]
+smalldatashreya4<-droplevels(smalldatashreya4)
+
+smalldatashreya4<-na.omit(smalldatashreya4)
+ggplot(smalldatashreya4,aes(x=year,y=fehome,col=govtjob))+
+  geom_smooth()+scale_colour_brewer(palette = "Set1")+
+  theme(legend.position="bottom")
+
+smalldatashreya4b <-  smalldatashreya4[smalldatashreya4$year == 1974 | smalldatashreya4$year == 1998, ]
+smalldatashreya4b$late <- as.numeric(smalldatashreya4b$year == 1998)
+modelshreya4 <- lm(fehome~govtjob*late,smalldatashreya4b)
+stargazer(modelshreya4,header=FALSE,title="",type="text")
 
 # Religion Dummy Variables
 get_labels(gss$relig)
@@ -106,7 +85,7 @@ table(gss$attendnum)
 # Time
 gss$late <- as.numeric(gss$year >= 1992)
 
-# Respondent Income 
+# Respondent Income in Thousands Constant Dollars
 get_label(gss$conrinc)
 summary(gss$conrinc)
 gss$conrincth <- gss$conrinc/1000
